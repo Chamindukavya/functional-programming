c4f49580d8b8101c1f7245b91e1fbcfa76888a86
Gawasingha Arachchige Chamindu Kavya <162815316+Chamindukavya@users.noreply.github.com>
2025-02-14 10:41:21 +0530

    add functional list structure and message handling

--- public/first-class.js
+++ public/first-class.js
@@ -11,14 +11,28 @@ const add = function(x,y){
 const sum = add(3,4) 
 console.log(sum)
 
+
+
 const greet = function(salutation){
     return function(name){
         return `${salutation} ${name}`
     }
 }
-
 const hello = greet('Hello')
-
 const message = hello('John')
-
 console.log(message)
+
+
+const numbers = [1,2,3,4,5]
+numbers.forEach(function(number){
+    console.log(number)
+})
+
+// const output = function(number){    
+//     console.log(number)
+// }
+
+// above out function can be simplified as below
+const output = number => console.log(number)
+
+numbers.forEach(output)
